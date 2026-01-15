f3d27050cb21999382b31b6a1dc8969028e941e8
kkonanjeanluc2-debug <kkonanjeanluc2@gmail.com>
2026-01-15 18:38:27 +0000

    Updated AppSidebar.vue

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -64,7 +64,7 @@ const menuStructure = computed((): MenuCategory[] => {
   // Ventes
   const ventesItems = [];
   if (tabsMap.has('reports')) ventesItems.push({ ...tabsMap.get('reports')! });
-  if (tabsMap.has('Clients')) ventesItems.push({ ...tabsMap.get('Prospects')! });
+  if (tabsMap.has('Clients')) ventesItems.push({ ...tabsMap.get('')! });
   if (tabsMap.has('tracking')) ventesItems.push({ ...tabsMap.get('tracking')! });
   if (tabsMap.has('orders')) ventesItems.push({ ...tabsMap.get('orders')! });
 
