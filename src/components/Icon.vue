fb88329a861cbb716b362f7c611db0aa61fea36f
kkonanjeanluc2-debug <kkonanjeanluc2@gmail.com>
2026-02-13 23:19:13 +0000

    Updated Icon.vue

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -356,5 +356,26 @@ defineProps<{
       stroke-width="2"
       d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z M9 3v5a2 2 0 002 2h5"
     />
+    <path
+      v-else-if="name === 'print'"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+    />
+    <path
+      v-else-if="name === 'check'"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M5 13l4 4L19 7"
+    />
+    <path
+      v-else-if="name === 'close'"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M6 18L18 6M6 6l12 12"
+    />
   </svg>
 </template>
